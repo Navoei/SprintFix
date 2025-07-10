e39d808fab4bed19a3705d2fd5def041270e0919
Navoei <SuperNavoeiMC@gmail.com>
2025-07-10 13:38:35 -0500

    Removed Useless Lines

--- src/client/java/me/navoei/sprintfix/client/SprintFixClient.java
+++ src/client/java/me/navoei/sprintfix/client/SprintFixClient.java
@@ -1,19 +1,12 @@
 package me.navoei.sprintfix.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.resources.ResourceLocation;
 
 public class SprintFixClient implements ClientModInitializer {
-
-    public static final String MOD_ID = "sprintfix";
 
     @Override
     public void onInitializeClient() {
 
-    }
-
-    public static ResourceLocation id(String path) {
-        return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
     }
 
 }
