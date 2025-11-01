85b25f418c8d9a527dccf23e7c9a10c1a72cdeb8
Navoei <SuperNavoeiMC@gmail.com>
2025-11-01 17:44:43 -0500

    Updates to the Mixin
    Again, thanks to @lowercasebtw for his suggestions.

--- src/main/java/me/navoei/sprintfix/mixin/SprintFixClientMixin.java
+++ src/main/java/me/navoei/sprintfix/mixin/SprintFixClientMixin.java
@@ -1,20 +1,21 @@
 package me.navoei.sprintfix.mixin;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LocalPlayer.class)
-public class SprintFixClientMixin {
+public abstract class SprintFixClientMixin {
+
+    @Shadow
+    public abstract boolean isUsingItem();
 
     @Inject(method = "shouldStopRunSprinting", at = @At("RETURN"), cancellable = true)
-    protected void injectSprintFixMethod(CallbackInfoReturnable<Boolean> cir) {
-        LocalPlayer player = Minecraft.getInstance().player;
-        if (player==null) return;
-        if (player.isUsingItem()) cir.setReturnValue(true);
+    protected void sprintfix$injectSprintFixMethod(CallbackInfoReturnable<Boolean> cir) {
+        if (this.isUsingItem()) cir.setReturnValue(true);
     }
 
 }
